0d3f508af530fa3da5070577ed1facd99e4e7b7b
Shreyas Garimella <shreyas.g197@gmail.com>
2022-08-05 17:42:43 -0700

    add random generating computerChoice and prompt for player choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+const options = ["Rock", "Paper", "Scissors"]; //defining array and options for computer to choose from
+let getComputerChoice = options[Math.floor(Math.random() * options.length)]; //variable for random computer choice
+console.log(getComputerChoice);
+
+let playerChoice1 = prompt("Please choose Rock/Paper/Scissors", ""); //inputting player choice
+
+function oneRound(choice1, choice2) {
+    if (choice1 === choice2) {
+        alert("It's a tie!");
+    }
+    else if 
+
+}
