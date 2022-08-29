256a2bd1c5221a9f6c064f8e5ddacf2bbe4263eb
Shreyas Garimella <shreyas.g197@gmail.com>
2022-08-29 02:41:11 -0700

    add more functionality to results and user clarity

--- javascript.js
+++ javascript.js
@@ -1,54 +1,97 @@
-//function to play one round of Rock/Paper/Scissors
-function oneRound() {
+let playerChoice; //defining variable for player choice
+let playerWin = 0; //defining variable for player wins
+let playerLoss = 0; //defining variable for player losses
+
+function oneRound(playerChoice) {
     const options = ["rock", "paper", "scissors"]; //defining array and options for computer to choose from
     let getComputerChoice = options[Math.floor(Math.random() * options.length)]; //variable for random computer choice
-    console.log(getComputerChoice);
+    compChoice.textContent = "The computer chose " + getComputerChoice + "!"; //display the computer choice to user in p tag
+        if (playerChoice === getComputerChoice) { //if condition for a tie
+            roundResults.textContent = "It's a tie!";
+            Wins.textContent = playerWin;
+            Losses.textContent = playerLoss;
+            return;
+        } else if ((playerChoice === "rock") && (getComputerChoice === "scissors")) {
+            roundResults.textContent = "You win this round!";
+            playerWin = playerWin + 1; //modifying variable keeping track of player wins
+            Wins.textContent = playerWin;
+            Losses.textContent = playerLoss;
+            return;
+        } else if ((playerChoice === "paper") && (getComputerChoice === "rock")) {
+            roundResults.textContent = "You win this round!";
+            playerWin = playerWin + 1; //modifying variable keeping track of player wins
+            Wins.textContent = playerWin;
+            Losses.textContent = playerLoss;
+            return;
+             //player win if statement
+        } else if ((playerChoice === "scissors") && (getComputerChoice ==="paper")) {
+            roundResults.textContent = "You win this round!";
+            playerWin = playerWin + 1; //modifying variable keeping track of player wins
+            Wins.textContent = playerWin;
+            Losses.textContent = playerLoss;
+            return;
+        } else {
+            roundResults.textContent = "Womp womp, you lose this one...Better luck next round";
+            playerLoss = playerLoss + 1; //modifying variable keeping track of computer wins
+            Wins.textContent = playerWin;
+            Losses.textContent = playerLoss;
+            return;
+        }
+    };
 
-    let playerChoice1 = prompt("Please choose Rock/Paper/Scissors", ""); //inputting player choice
-    let playerChoice = playerChoice1.toLowerCase(); //converting whatever the player inputs to be case insensitive
-    alert("The computer chose " + getComputerChoice); //alert indicating what the computer chose
+const rockBtn = document.querySelector('#rock'); //adding an event listener for the Rock button and executing code below assuming player choice is rock
+const paperBtn = document.querySelector('#paper'); //adding an event listener for the Paper button and executing code below assuming player choice is paper
+const scissorsBtn = document.querySelector('#scissors'); //adding an event listener for the Scissors button and executing code below assuming player choice is scissors
+const choice = document.querySelector('#choice'); //adding a selector for a p tag to display user choice to the user for clarity
+const compChoice = document.querySelector('#compChoice'); //adding a selector for a p tag to display computer choice to the user
 
-    if (playerChoice === getComputerChoice) {
-        alert("It's a tie!");
-        return; //code for a tie
-    } else if ((playerChoice === "rock") && (getComputerChoice === "scissors")) {
-        alert("You win!");
-        playerWin = playerWin + 1; //modifying variable keeping track of player wins
-        return; //player win if statement
-    } else if ((playerChoice === "paper") && (getComputerChoice === "rock")) {
-        alert ("You win!");
-        playerWin = playerWin + 1; //modifying variable keeping track of player wins
-        return; //player win if statement
-    } else if ((playerChoice === "scissors") && (getComputerChoice ==="paper")) {
-        alert("You win!");
-        playerWin = playerWin + 1; //modifying variable keeping track of player wins
-        return; //player win if statement
+
+rockBtn.addEventListener("click", function(){ //function called when button is clicked
+    if (playerWin == 5 || playerLoss == 5) {
+        return; //prevents function from executing if player win or computer win are at 5
     } else {
-        alert("Womp womp, you lose...better luck next time!");
-        playerLoss = playerLoss + 1; //modifying variable keeping track of computer wins
-        return; //all other conditions, loss
+        theGame("rock");
     }
+});
+
+paperBtn.addEventListener("click", function(){
+    if (playerWin == 5 || playerLoss == 5) {
+        return;
+    } else {
+        theGame("paper");
+    }
+});
+
+scissorsBtn.addEventListener("click", function(){
+    if (playerWin == 5 || playerLoss == 5) {
+        return;
+    } else {
+        theGame("scissors");
+    }
+});
+
+const Wins = document.querySelector('#Wins'); //adding a selector to later modify text content of Wins
+const Losses = document.querySelector('#Losses'); //adding a selector to later modify text content of Losses
+const roundResults = document.querySelector('#roundResults');
+
+function theGame(playerChoice) {
+    choice.textContent = "You chose "+ playerChoice+"!"; //display the player choice
+    oneRound(playerChoice); //calling function to play one round of rock/paper/scissors
+    setTimeout(() => { //timeout to display updated results in DOM before alert
+        if (playerWin == 5) {
+            if (confirm("You have won the game! Try again?") == true) {
+                location.reload(); //reloads the page is user wants to try again
+            } else {
+            return;
+            }
+        } else if (playerLoss == 5) {
+            if (confirm("You have lost the game! Try again?") == true) {
+                location.reload();
+            } else {
+            return;
+            }
+        } else {
+            return;
+        }
+    }, 10) //interval time
 }
-
-//defining variables to keep track of player and computer wins in a global scope to call upon them in other functions
-let playerWin = 0;
-let playerLoss = 0;
-
-//making function to play 5 rounds of rock/paper/scissors and determine overall winner (player or computer)
-/*function game() {
-    for (let i=0; i<5; i++) {
-        oneRound();
-} 
-alert("You won " + playerWin + " rounds");
-alert("The computer won " + playerLoss + " rounds");
-if (playerWin > playerLoss) {
-    alert("You're the overall winner of the 5 rounds! Congrats!!");
-} else if (playerWin == playerLoss) {
-        alert("Overall, it's a tie!");
-    } else {
-        alert("The computer destroyed you overall. Better luck next time!");
-    }
-};
-
-game();
-*/
